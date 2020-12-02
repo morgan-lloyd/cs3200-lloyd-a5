33df1832014b706a7ec05b4a1c7919a312202182
Morgan Lloyd <morganlloyd@outlook.com>
2020-12-02 00:10:12 -0500

    final commit

--- daos/questions-dao.js
+++ daos/questions-dao.js
@@ -1,5 +1,5 @@
 const questionsModel = require('../models/questions/questions-model.js')
 const findAllQuestions = () => questionsModel.find()
-const findQuizById = (qId) => questionsModel.findById(qId)
-const findQuestionsForQuiz = (qid) => questionsModel.find({quizId:qId})
-module.exports = { findAllQuizzes, findQuizById, findQuestionsForQuiz }
+const findQuestionById = (qId) => questionsModel.findById(qId)
+const findQuestionsForQuiz = (qzId) => questionsModel.find({quizId: qzId})
+module.exports = {findAllQuestions, findQuestionById, findQuestionsForQuiz }
--- models/questions/questions-schema.js
+++ models/questions/questions-schema.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const questionSchema = mongoose.Schema({
    question: String,
-   options: [String]
+   options: [String],
    quizId: String
 }, {collection: 'questions'})
 module.exports = questionSchema
